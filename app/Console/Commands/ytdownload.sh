9980b4c0fd809bf311717f85a0e7e89c88b47eaa
pzq2017 <499839055@qq.com>
2017-12-21 14:00:30 +0800

    update file

--- app/Console/Commands/ytdownload.sh
+++ app/Console/Commands/ytdownload.sh
@@ -3,17 +3,21 @@
 #$1 远程文件目录  /A/B
 #$2 远程文件名   a.xml  or /C/a.xml
 #$3 本地文件目录   /D
-if [ $# -ne 3 ]; then
-    echo "usage: sh ytdownload.sh [Destination Folder] [Destination File] [Origin Folder]"
+#$4 YouTube账户
+#$5 YouTube秘钥文件名
+if [ $# -ne 5 ]; then
+    echo "usage: sh ytdownload.sh [Destination Folder] [Destination File] [Origin Folder] [YouTube account name] [ssh key filename]"
     echo "       [Destination Folder]: / or /A/B"
     echo "       [Destination File]: a.xml or /C/a.xml"
     echo "       [Origin Folder]: / or /D"
+    echo "       [YouTube account name]"
+    echo "       [ssh key filename]"
     exit 1
 fi
 if [[ "$1" == "/" ]]; then
-    ascp -P 33001 -i ~/.ssh/Youtube\ 8sian --src-base=$1 asp-8sian2@asia.aspera.googleusercontent.com:$1$2 $3
+    ascp -P 33001 -i ~/.ssh/Youtube\ $5 --src-base=$1 $4@asia.aspera.googleusercontent.com:$1$2 $3
 else
-    ascp -P 33001 -i ~/.ssh/Youtube\ 8sian --src-base=$1 asp-8sian2@asia.aspera.googleusercontent.com:$1/$2 $3
+    ascp -P 33001 -i ~/.ssh/Youtube\ $5 --src-base=$1 $4@asia.aspera.googleusercontent.com:$1/$2 $3
 fi
 if [ $? -eq 0 ]; then
 	echo $2
--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,25 @@
 |
 */
 
-Route::redirect('/', '/manage');
+Auth::routes();
 
 Route::get('/manage', function () {
     return view('manage');
 })->name('manage')->middleware('auth');
 
-Auth::routes();
-
 Route::post('/admin/publish', 'AdminController@publish')->name('admin.publish');
-
 Route::resource('admin', 'AdminController');
 
 Route::get('/task/lists', 'TaskController@lists')->name('task.lists');
-
 Route::get('/task/download', 'TaskController@download');
-
 Route::resource('task', 'TaskController', [
     'except' => ['create', 'show']
+]);
+
+Route::resource('genfile', 'GenFileController', [
+    'only' => ['index', 'store']
 ]);
 
 Route::get('/csv_temp/{one?}', function () {
     App\Util\FileHandle::fileStorageShow();
 });
-
-Route::resource('genfile', 'GenFileController', [
-    'only' => ['index', 'store']
-]);
