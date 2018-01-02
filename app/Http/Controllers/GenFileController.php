b91a4fc3c7cf732c8368781460cb84a5cb028984
SunD1ng <sunding1992@gmail.com>
2018-01-02 18:28:34 +0800

    according agent change encoding

--- app/Http/Controllers/GenFileController.php
+++ app/Http/Controllers/GenFileController.php
@@ -350,12 +350,45 @@ class GenFileController extends Controller
                     $num = 0;
                 }
                 $row = $data[$i];
-                foreach ($row as $key => $value) {
-                    $row[$key] = mb_convert_encoding($value, 'gbk', 'utf-8');
+                if ($this->getOS() == 'mac') {
+                    foreach ($row as $key => $value) {
+                        $row[$key] = mb_convert_encoding($value, 'utf-8');
+                    }
+                } else {
+                    foreach ($row as $key => $value) {
+                        $row[$key] = mb_convert_encoding($value, 'gbk', 'utf-8');
+                    }
                 }
                 fputcsv($fp, $row);
             }
         }
         fclose($fp);
+    }
+
+    public function getOS()
+    {
+        $agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+        if (strpos($agent, 'windows nt')) {
+            $platform = 'windows';
+        } elseif (strpos($agent, 'macintosh')) {
+            $platform = 'mac';
+        } elseif (strpos($agent, 'ipod')) {
+            $platform = 'ipod';
+        } elseif (strpos($agent, 'ipad')) {
+            $platform = 'ipad';
+        } elseif (strpos($agent, 'iphone')) {
+            $platform = 'iphone';
+        } elseif (strpos($agent, 'android')) {
+            $platform = 'android';
+        } elseif (strpos($agent, 'unix')) {
+            $platform = 'unix';
+        } elseif (strpos($agent, 'linux')) {
+            $platform = 'linux';
+        } else {
+            $platform = 'other';
+        }
+
+        return $platform;
     }
 }
