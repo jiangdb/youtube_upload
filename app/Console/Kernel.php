48a13eb2a323114c2586d2dc330097c99ed67e47
pzq2017 <499839055@qq.com>
2018-01-04 18:35:23 +0800

    temp test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('task:handle')->everyMinute();
+        $schedule->command('task:handle')->everyThirtyMinutes();
     }
 
     /**
