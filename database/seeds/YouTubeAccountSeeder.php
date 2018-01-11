97967f0775d03dd53f4ad5444251eacee8bb6fec
pzq2017 <499839055@qq.com>
2018-01-11 17:45:04 +0800

    add new youtube account

--- database/seeds/YouTubeAccountSeeder.php
+++ database/seeds/YouTubeAccountSeeder.php
@@ -17,6 +17,14 @@ class YouTubeAccountSeeder extends Seeder
             'display_name' => '8sian',
             'account_name' => 'asp-8sian2',
             'ssk_key_filename' => 'Youtube_8sian',
+        ], [
+            'display_name' => 'smg',
+            'account_name' => 'asp-kankannews2',
+            'ssk_key_filename' => 'Youtube_smg',
+        ], [
+            'display_name' => 'zjstv',
+            'account_name' => 'asp-zhejiang2',
+            'ssk_key_filename' => 'Youtube_zjstv',
         ]);
 
         foreach ($account as $key => $value) {
